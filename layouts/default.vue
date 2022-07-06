0d4f944b3cbad534f7fa060201fccefb6bac14f5
sammitako <p.hansaem@hotmail.com>
2022-07-06 12:31:40 +0900

    layouts -> default, error

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <Header />
+  <slot />
+  <Footer />
+</template>
+<script>
+import Header from "~/components/Header";
+import Footer from "~/components/Footer";
+export default {
+  components: {
+    Header,
+    Footer,
+  },
+};
+</script>
+
+<style lang="scss">
+/* @import "~/scss/main"; */
+</style>
